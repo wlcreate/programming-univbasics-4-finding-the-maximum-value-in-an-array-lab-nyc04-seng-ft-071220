5ef98cd1fea8ee222fad10dccbde7c6717143b30
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 22:06:30 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,4 @@
 def find_max_value(array)
-  # Add your solution here
-  count = 0
-  while count < array.length do
-    
-end
-
-def find_max_value(array)
   sorted_array = array.sort
-  count = 0
-  while count < sorted_array do
-    
+  sorted_array.last
+end    
